6d53006eec318bddca1345785beb24bc2b5990fd
test-user <test@email.com>
2020-04-04 00:55:41 +0900

    amazoncorretto8 (under construction)

---
+++ amazoncorretto8/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 実行スクリプトのpath取得
+# https://qiita.com/koara-local/items/2d67c0964188bba39e29
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+cd $SCRIPT_DIR
+
+# symbolicの実体を辿ってimageをbuild
+# ref: https://blog.kkty.jp/entry/2019/06/16/214951
+tar -czh . | docker build \
+        -t jupyter_conda_custom_amazoncorretto8 \
+        --build-arg BASE_IMAGE=amazoncorretto:8 \
+        --build-arg USER_NAME=user \
+        --build-arg USER_UID=1000 \
+        --build-arg PASSWD="" \
+        --build-arg SPARK_VERSION=2.4.5 \
+        --build-arg HADOOP_VERSION=2.7 \
+        - 
